a10a9f390f907c5b3fc09be8f15e80d50b01f9fa
pk1988 <phanikumar.narina@magikminds.com>
2016-09-09 14:36:01 +0530

    creating angular PWA

---
+++ passportApp/resources/js/app.js
@@ -0,0 +1,99 @@
+/***
+Marblesz AngularJS App Main Script
+***/
+/* Marblesz App */
+var passportApp = angular.module('passportApp',[
+  'ui.router',
+  'ui.bootstrap',
+  'passportApp.constants'
+]);
+
+passportApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/main");
+    
+    $stateProvider
+    .state('main',{
+      url: "/main",
+      templateUrl: "views/mainpage.html"
+    })
+
+    .state('home',{
+      url: "/home",
+      templateUrl: "views/home.html"
+    })
+
+    .state('track_application_status',{
+      url: "/track_application_status",
+      templateUrl: "views/track_application_status.html"
+    })
+
+    .state('track-shipping-status',{
+      url: "/track-shipping-status",
+      templateUrl: "views/track-shipping-status.html"
+    })
+
+    .state('faq',{
+      url: "/faq",
+      templateUrl: "views/faq.html"
+    })
+
+    .state('feedback',{
+      url: "/feedback",
+      templateUrl: "views/feedback.html"
+    })
+
+    .state('complaint',{
+      url: "/complaint",
+      templateUrl: "views/complaints.html"
+    })
+
+    .state('privacy-policy',{
+      url: "/privacy-policy",
+      templateUrl: "views/privacyPolicy.html"
+    })
+
+    .state('terms-and-conditions',{
+      url: "/terms-and-conditions",
+      templateUrl: "views/terms-and-conditions.html"
+    })
+
+    .state('holiday-list',{
+      url: "/holiday-list",
+      templateUrl: "views/holiday-list.html"
+    })
+
+    .state('passport-status-history',{
+      url: "/passport-status-history",
+      templateUrl: "views/passport-status-history.html"
+    })
+
+    .state('passport-status-history-received',{
+      url: "/passport-status-history-received",
+      templateUrl: "views/passport-status-history-received.html"
+    })
+
+    .state('ckgs-application-center',{
+      url:"/ckgs-application-center",
+      templateUrl: "views/ckgs-application-center.html"
+    })
+
+    .state('ckgs-application-location-map',{
+      url: "/ckgs-application-location-map",
+      templateUrl: "views/ckgs-application-location-map.html"
+    })
+
+
+  });
+
+/* Init global settings and run the app */
+passportApp.run(["$rootScope", "$state", function($rootScope, $state) {
+    $rootScope.$state = $state; // state to be accessed from view
+}]);
+
+passportApp.run(function($rootScope, $templateCache) {
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+        if (typeof(current) !== 'undefined'){
+            $templateCache.remove(current.templateUrl);
+        }
+    });
+});
---
+++ passportApp/serviceWorker.js
@@ -0,0 +1,93 @@
+var cacheName = 'PWA-passport';
+var filesToCache = [
+  "/",
+  "index.html",
+  "views/mainpage.html",
+  "views/home.html",
+  "views/navbar.html",
+  "views/track_application_status.html",
+  "views/track-shipping-status.html",
+  "views/feedback.html",
+  "views/complaints.html",
+  "views/faq.html",
+  "views/globalHeader.html",
+  "views/commonfooter.html",
+  "views/privacyPolicy.html",
+  "views/terms-and-conditions.html",
+  "resources/css/style.css",
+  "resources/css/media.css",
+  "resources/css/plugin.css",
+  "resources/js/main.js",
+  "resources/js/app.js",
+  "resources/images/parking-map.jpg",
+  "resources/images/rate-our-app.jpg",
+  "resources/images/view-map.jpg",
+  "resources/images/common/ckgs-logo.png",
+  "resources/images/common/ckgs-logo-sm copy.png",
+  "resources/images/common/ckgs-logo-sm.png",
+  "resources/images/common/ckgs-logo-sm_1.png",
+  "resources/images/common/ckgs-logo-sm-bkp.png",
+  "resources/images/common/ckgs-white-logo.png",
+  "resources/images/common/close-btn.png",
+  "resources/images/common/custom-back.png",
+  "resources/images/common/custom-comment.png",
+  "resources/images/common/feedback-icon.png",
+  "resources/images/common/feedback-icon-btm.png",
+  "resources/images/common/fedex-img.jpg",
+  "resources/images/common/fedex-img-1.jpg",
+  "resources/images/common/feedback-icon-hov.png",
+  "resources/images/common/feedback-icon-sm.png",
+  "resources/images/common/holiday-list-icon.png",
+  "resources/images/common/icon-history-steps.png",
+  "resources/images/common/logout-icon.png",
+  "resources/images/common/Untitled-1.png",
+  "resources/images/common/retina/ckgs-white-logo.png",
+  "resources/images/common/retina/close-btn.png",
+  "resources/images/common/retina/custom-back.png",
+  "resources/images/common/retina/feedback-icon.png",
+  "resources/images/common/retina/feedback-icon-btm.png",
+  "resources/images/common/retina/feedback-icon-lg.png",
+  "resources/images/common/retina/holiday-list-icon.png",
+  "resources/images/common/retina/icon-history-steps.png",
+  "resources/images/common/retina/logout-icon.png",
+  "launcher-icon-0-75x.png",
+  "launcher-icon-1-5x.png",
+  "launcher-icon-1x.png",
+  "launcher-icon-2x.png",
+  "launcher-icon-3x.png",
+  "launcher-icon-4x.png"
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
