30a437bcc536bbca04b405e777ec23c85a2e90e1
pk1988 <phanikumar.narina@magikminds.com>
2016-09-12 14:17:09 +0530

    Login Controller

---
+++ CKGS-PWA/resources/js/controllers/ckgs-loginCtrl.js
@@ -0,0 +1,5 @@
+'use strict';
+var app = angular.module('ckgsPWA').
+controller('LoginController', ['$rootScope','$scope', '$window', '$state', '$http', '$timeout', 'CONSTANTS', function($rootScope, $scope, $window, $state, $http, $timeout, CONSTANTS) {
+	console.log("LoginPAGE");
+}]);
